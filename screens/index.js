a281f340c33ad0e55681fa94528787a99fd3b9f9
Yong Quan <yongquan02@gmail.com>
2023-03-21 23:45:43 +0800

    bruh

--- screens/index.js
+++ screens/index.js
@@ -1,7 +1,7 @@
 //import all the screens from the screens folder and export them
 import LoginScreen from "./loginScreen";
-import Map from "./map"
-import MyComponent from "./map1"
+import Map from "./map";
+import MyComponent from "./map1";
 
 export {
   LoginScreen,
