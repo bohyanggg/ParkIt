820b144fd0619314394e08a17cc42eab876dcb6a
YQ0195 <103987256+YQ0195@users.noreply.github.com>
2023-04-10 03:38:22 +0800

    updated map
    able to search for carparks and mark desired carparks on map, waiting to geocode location into coordinates and filter carparks within certain range

--- screens/map.js
+++ screens/map.js
@@ -1,4 +1,197 @@
 import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  Text,
+  FlatList,
+} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import { SearchBar } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
+import proj4 from 'proj4';
+export default function Map() {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [data, setData] = useState([]);
+  const [token, setToken] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const navigation = useNavigation();
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [selectedCoordinates, setSelectedCoordinates] = useState(null);
+  const svy21Projection = '+proj=tmerc +lat_0=1.366666666666667 +lon_0=103.8333333333333 +k=1 +x_0=28001.642 +y_0=38744.572 +ellps=WGS84 +units=m +no_defs';
+  const wgs84Projection = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
+
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation(location);
+    })();
+  }, []);
+
+  useEffect(() => {
+    const accessKey = 'acb2ead0-8cef-46a5-af01-15d850b437ce';
+    const tokenUrl = 'https://www.ura.gov.sg/uraDataService/insertNewToken.action';
+
+    const getToken = async () => {
+      try {
+        const response = await axios.post(tokenUrl, null, {
+          headers: {
+            'AccessKey': accessKey
+          }
+        });
+        setToken(response.data.Result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const interval = setInterval(() => {
+      getToken();
+    }, 24 * 60 * 60 * 1000);
+
+    getToken();
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const url = 'https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Details';
+    const accessKey = 'acb2ead0-8cef-46a5-af01-15d850b437ce';
+
+    axios.get(url, {
+      headers: {
+        'AccessKey': accessKey,
+        'Token': token
+      }
+    })
+      .then(response => {
+        const data = response.data.Result;
+        setData(data.filter((item, index, self) =>
+          index === self.findIndex((t) => (
+            t.ppCode === item.ppCode
+          ))
+        ));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [token]);
+
+  const handleSearch = (text) => {
+    setSearchTerm(text);
+  };
+
+  const filteredData = data.filter((item) => {
+    const ppNameMatch = item.ppName.toLowerCase().includes(searchTerm.toLowerCase());
+    const geoMatch = item.geometries.some((geo) =>
+      geo.coordinates.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    return ppNameMatch || geoMatch;
+  });
+
+  const handleSelect = (coordinates) => {
+    const [x, y] = coordinates.split(',');
+    const svy21Coordinates = [parseFloat(x), parseFloat(y)];
+    const wgs84Coordinates = proj4(svy21Projection, wgs84Projection, svy21Coordinates);
+    setSelectedCoordinates({ latitude: wgs84Coordinates[1], longitude: wgs84Coordinates[0] });
+  };
+
+  const renderItem = ({ item }) => (
+    <View key={item.ppCode}>
+      <Text>Parking Lot: {item.ppName}</Text>
+      <Text>Available Lots: {item.parkCapacity}</Text>
+      <Text>Weekday Rate: {item.weekdayRate}</Text>
+      <Text>Saturday Rate: {item.satdayRate}</Text>
+      <Text>Sunday/PH Rate: {item.sunPHRate}</Text>
+      <Text>Parking System: {item.parkingSystem}</Text>
+      <Text>Coordinates:</Text>
+      {item.geometries.map((geo, index) => (
+        <Button
+          key={`${item.carparkNo}-${item.lotType}-geo-${index}`}
+          title={`Select`}
+          onPress={() => handleSelect(geo.coordinates)}
+        />
+      ))}
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <SearchBar
+        placeholder="Search Destination"
+        onChangeText={handleSearch}
+        value={searchTerm}
+        containerStyle={{backgroundColor: '#fff'}}
+        inputContainerStyle={{ backgroundColor: '#eee' }}
+        inputStyle={{ fontSize: 16 }}
+      />
+
+      <View style={styles.mapContainer}>
+        {currentLocation && (
+          <MapView
+  style={styles.map}
+  initialRegion={{
+    latitude: currentLocation.coords.latitude,
+    longitude: currentLocation.coords.longitude,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }}
+>
+  <Marker coordinate={currentLocation.coords} />
+  {console.log('Selected coordinates:', selectedCoordinates)}
+  {selectedCoordinates && (
+    <Marker coordinate={selectedCoordinates} pinColor="green" />
+  )}
+</MapView>
+        )}
+      </View>
+
+      <View style={styles.listContainer}>
+        <FlatList
+          data={filteredData}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => item.ppCode + index}
+        />
+      </View>
+
+      <View>
+        <Button
+          title="Back"
+          color="blue"
+          onPress={() => navigation.navigate("Login")}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mapContainer: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+  listContainer: {
+    flex: 1,
+  },
+});
+
+/*import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
