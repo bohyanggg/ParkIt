cb7b4e313fa96bdc7b54527d1dd0b870b206d1f6
limx0139 <123717942+limx0139@users.noreply.github.com>
2023-04-10 20:27:53 +0800

    Update settingsScreen.js

--- screens/settingsScreen.js
+++ screens/settingsScreen.js
@@ -1,10 +1,10 @@
 // can just implement user profile and logout
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import { auth } from '../firebase/firebaseconfig';
 
-const SettingsScreen = ({navigation}) => {
-  
+const SettingsScreen = ({ navigation }) => {
+
   const handleSignOut = () => {
     auth
       .signOut()
@@ -16,46 +16,69 @@ const SettingsScreen = ({navigation}) => {
 
   return (
     <View style={styles.container}>
-          <View style={styles.navigators}>
-            <Text>
-              This is the settings page
-            </Text>
-            <Button
-              title="Profile Page"
-              color="#5D0EEA"
-              onPress={() => { navigation.navigate("Profile") }}
-            />
-            <Text>
-              The account you are in: {auth.currentUser?.email} 
-            </Text>
-            <Button
-              title="Sign Out"
-              color="#5D0EEA"
-              onPress={handleSignOut}
-              //onPress={() => { navigation.navigate("Login") }}
-            />
-          </View>
+
+      <View style = {styles.titleview} >
+      <Text style={styles.title}>Settings</Text>
+      </View>
+      
+<View style = {{    alignItems: 'center',justifyContent: 'center',}}>
+      <View style={{ paddingBottom: 20 }}>
+        <Image source={require('../assets/images/manuser.png')} style={styles.image} />
+      </View>
+      <View style={styles.navigators}>
+
+        <Button
+          title="Edit Profile Info"
+          color="#5D0EEA"
+          onPress={() => { navigation.navigate("Profile") }}
+        />
+        <Text>
+          The account you are in: {auth.currentUser?.email}
+        </Text>
+        <Button
+          title="Sign Out"
+          color="#5D0EEA"
+          onPress={handleSignOut}
+        //onPress={() => { navigation.navigate("Login") }}
+        />
+      </View>
     </View>
-      
-      );}
-      
-      const styles = StyleSheet.create({
-        container: {
-          flex: 1000,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center'
-          },
-        navigators: {
-          marginVertical: 2,
-          paddingHorizontal:'20%',
-          flex: 1,
-          },
-        buttons: {
-          marginVertical: 8,
-          flexDirection:'row',
-          paddingTop:20,
-          },
-      });
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  navigators: {
+    marginVertical: 2,
+    paddingHorizontal: '20%',
+    //flex: 1,
+  },
+  buttons: {
+    marginVertical: 8,
+    flexDirection: 'row',
+    paddingTop: 20,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+    // backgroundColor: '#0a9',
+    padding: 5,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    
+  },
+  titleview:{
+    padding:50,
+    paddingBottom: 100,
+
+    
+  }
+});
 
 export default SettingsScreen;
