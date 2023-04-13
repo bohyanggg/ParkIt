dc65781c507243a22b95bd34b3bf448ea9522ea5
BruceLi <75286561+BruceLee7@users.noreply.github.com>
2023-04-13 21:46:19 +0800

    Update App.js

--- App.js
+++ App.js
@@ -7,11 +7,13 @@ import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import {
   LoginScreen,
   Map,
+  mapWithoutLogin,
   CarParkScreen,
   CPDetails,
   ForgetPasswordScreen,
   SignUpScreen,
   ProfileScreen,
+  Html
 } from "./screens";
 
 import MainContainer from "./screens/MainContainer"
@@ -25,6 +27,8 @@ function App() {
         <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false,}}/>
         <Stack.Screen name="MainContainer" component={MainContainer} options={{headerShown: false,}} />
         <Stack.Screen name="Map" component={Map} />
+        <Stack.Screen name="mapWithoutLogin" component={mapWithoutLogin} options={{headerShown: false,}} />
+        <Stack.Screen name="Sample List of Car Parks" component={Html} />
         <Stack.Screen name="Carpark" component={CarParkScreen} options={{headerShown: false,}} />
         <Stack.Screen name="CarparkDetails" component={CPDetails} options={{headerShown: false,}} />
         <Stack.Screen name="ForgetPassword" component={ForgetPasswordScreen}   />
