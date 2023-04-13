1901c589bdf822bb28db3c8a2145efb57bce1315
BruceLi <75286561+BruceLee7@users.noreply.github.com>
2023-04-13 21:40:48 +0800

    Update and rename screens/mapp.js to mapWithoutLogin.js

---
+++ mapWithoutLogin.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+import { KeyboardAvoidingView, StyleSheet, Text, View, Button, SafeAreaView, Alert, TextInput, Image, Pressable, Keyboard, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
+
+export default function Map({ route, navigation }) {
+
+  const { start, end } = route.params;
+  const url = `https://www.google.com/maps/dir/${start}/${end}?hl=en`;
+
+  return (
+    <View style={styles.container}>
+      <WebView source={{ uri: url }} style={styles.map} />
+      <Button
+            title="HomePage"
+            color="#5D0EEA"
+            onPress={() => { navigation.replace("Login") }}
+            /> 
+    </View>
+    
+    
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  map: {
+    flex: 1,
+  },
+  navigators: {
+    marginVertical: 2,
+    paddingHorizontal:'20%',
+    flex: 1,
+  },
+});
+
+
